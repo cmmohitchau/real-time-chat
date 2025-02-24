679243670267ed6e2a942f6e7b4c14cfde9567eb
cmmohitchau <cmmohitchau@gmail.com>
2025-02-24 19:03:37 +0530

    add message route

---
+++ backend/src/routes/messageRoutes.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { protectRoute } from "../middleware/authMiddleware.js";
+import { getMessage, getUsersForSidebar, sendMessage } from "../controllers/messageController.js";
+
+const router = express.Router();
+
+router.get("/users" , protectRoute , getUsersForSidebar);
+
+router.get("/:id" , protectRoute , getMessage);
+
+router.post("/send/:id" , protectRoute , sendMessage);
+
+export default router;
