cc27c0b1cfafca41c09f7fdbe02189da5e62c655
cmmohitchau <cmmohitchau@gmail.com>
2025-03-22 10:28:37 +0530

    added some more functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,54 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Auth } from "./pages/Auth";
 import { Profile } from "./pages/Profile";
-import { Chat } from "./pages/Chat";
 import { HomePage } from "./pages/HomePage";
-import { useSelector } from "react-redux";
-import { RootState } from "./store";
 import { Navbar } from "./pages/Navbar";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "./store";
+import { useEffect } from "react";
+import axios from "axios";
+import { setUser, logout, setLoading } from "./store/slices/authSlice";
+import { Setting } from "./pages/Setting";
 
 function App() {
-  const { user } = useSelector( (state : RootState) => state.auth);
-  console.log(user);
-  
+  const dispatch = useDispatch();
+  const { user, loading } = useSelector((state: RootState) => state.auth);
+
+  // Check auth status on app load
+  useEffect(() => {
+    const checkAuth = async () => {
+      dispatch(setLoading(true)); // Explicitly set loading to true
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/auth/check`, {
+          withCredentials: true,
+        });
+        dispatch(setUser(response.data));
+      } catch (err) {
+        dispatch(logout());
+      } finally {
+        dispatch(setLoading(false)); // Always reset loading
+      }
+    };
+
+    if (!user) {
+      checkAuth();
+    }
+  }, [dispatch, user]);
+
+  // Show loading screen while checking auth
+  if (!user && loading) {
+    return <div className="h-screen flex items-center justify-center">Loading...</div>;
+  }
+
   return (
     <BrowserRouter>
-    <Navbar />
+      <Navbar />
       <Routes>
-        <Route path="/" element={user ?  <HomePage /> : < Navigate to="/Auth" />} />
-        <Route path="/Auth" element={  <Auth />  } />
-        <Route path="/profile" element={  <Profile />  } />
-        <Route path="/chat" element={user ? <Chat /> : < Navigate to="/Auth" />} />
+        <Route path="/" element={user ? <HomePage /> : <Navigate to="/Auth" />} />
+        <Route path="/Auth" element={<Auth />} />
+        <Route path="/profile" element={user ? <Profile /> : <Navigate to="/Auth" />} />
         <Route path="/*" element={<Navigate to="/" />} />
+        <Route path="/setting" element={<Setting />} />
       </Routes>
     </BrowserRouter>
   );
